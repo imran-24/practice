baa9c4ac132641ab46cf4db4a6945d6f56c35358
joe-nomad <1921731@iub.edu.bd>
2021-11-07 12:37:55 +0600

    sdd

--- project/settings.py
+++ project/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WAR NING: keep the secret key used in production secret!
-SECRET_KEY = os.environ['SECTER_KEY1234']
+SECRET_KEY = os.environ['SECTER1234']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
